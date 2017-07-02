08fdc1a79fbff791535eda9e620efc2f2a9d7098
turtsmcgurts <slizizy@gmail.com>
2017-07-02 16:27:37 -0400

    added "on_voice_state_update" event
    prints text when a user joins or leaves a voice channel

--- discord-bot.py
+++ discord-bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import asyncio
 import tokenfile #where we store my bots token code so it isn't on public display
 
@@ -11,5 +12,13 @@ async def on_ready():
     print(client.user.id)
     print('------')
     
+    
+#when somebody joins a voice channel
+@client.event
+async def on_voice_state_update(before, after):
+    #before The Member whose voice state changed prior to the changes
+    #after The Member whose voice state changed after the changes
+    print('{} joined voice channel "{}"'.format(after, after.voice.voice_channel))
+
     
 client.run(tokenfile.token)
