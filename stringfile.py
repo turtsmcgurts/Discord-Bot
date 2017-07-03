6320aec7ebc08e26ceb039a0043bd3487931ac4b
turtsmcgurts <slizizy@gmail.com>
2017-07-03 17:52:44 -0400

    added github link to help command

--- stringfile.py
+++ stringfile.py
@@ -1,7 +1,7 @@
 #this is here just so I don't clutter up the main file with shit. would be noticable if I expand on the project
 
 #HELP RESPONSES
-generic_help_response = 'Bot created by turtsmcgurts for general use.'
+generic_help_response = 'Bot created by turtsmcgurts for general use. https://github.com/turtsmcgurts/Discord-Bot'
 uses_help_response = 'Currently the only use is for giving voice channels their own private text channels, similar to mumble or any other VoIP really.'
 help_commands = 'For more detail: "!help textchannel"'
 textchannel_help_response = ('In order to add a text channel to a voice channel, create a text channel (name does not matter), then create '
